03195c7723eabd1e533e324d7fcd43b1c932ffe0
NohaHaneen <nohahaneensi@gmail.com>
2024-09-12 23:32:54 -0500

    setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <div className='app'>
-      Testing
+      checking client
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -5,3 +5,5 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 app.use(express.json);
+
+console.log("checking server");
