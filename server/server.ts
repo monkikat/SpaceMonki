839bae447f6d2249c57bc8256202a0289ec78f50
NohaHaneen <nohahaneensi@gmail.com>
2024-09-16 12:33:51 -0500

    port setup

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,9 @@
-console.log('hi testing');
+import * as express from 'express';
+import { PORT } from './utils/config';
+
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`server started on port: ${PORT}`);
+});
