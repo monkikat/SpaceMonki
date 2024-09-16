7df16c173ee58fa6a806b6af3ac48e818a8fc5d4
NohaHaneen <nohahaneensi@gmail.com>
2024-09-16 12:01:08 -0500

    backend setup with nodemon running ts

--- server/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import dotenv from "dotenv";
-
-/* CONFIGURATIONS */
-dotenv.config();
-const app = express();
-app.use(express.json);
-
-console.log("checking server");
