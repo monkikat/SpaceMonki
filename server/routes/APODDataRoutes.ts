d5039124cb292080a11cd79a94ff0b8117c918c2
NohaHaneen <nohahaneensi@gmail.com>
2024-10-01 13:45:28 -0500

    added endpoint for serverless cron job

--- server/routes/APODDataRoutes.ts
+++ server/routes/APODDataRoutes.ts
@@ -16,6 +16,7 @@ const {
 router.route('/').get(getTodayAPODHandler).post(addAPODDataHandler);
 router.route('/week').get(getWeeksAPODHandler);
 router.route('/randomAPOD').get(getRandomAPODHandler);
+router.route('/cronApi').get(fetchAndStoreAPODDataHandler);
 router.route('/apiTesting').get(massFetchAndStoreAPODDataHandler);
 router.route('/randomAPODS').get(getRandomAPODSHandler);
 router.route('/:date').get(getReqAPODHandler);
